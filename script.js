d9ccc2a9433dc2028aa56e92cf0f2bdf1b40123f
tottemo-K4NiC <setarei20200628.daisukiclub@gmail.com>
2025-06-26 14:24:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -134,3 +134,15 @@ mobileBtn.addEventListener('click', () => {
     window.scrollBy(0, 180);
   }, 200);
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const accordions = document.querySelectorAll(".accordion");
+  accordions.forEach(btn => {
+    btn.addEventListener("click", () => {
+      btn.classList.toggle("active");
+      const panel = btn.nextElementSibling;
+      panel.style.display = panel.style.display === "block" ? "none" : "block";
+    });
+  });
+});
